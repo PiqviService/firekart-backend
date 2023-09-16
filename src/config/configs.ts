799c762f319d7d5c464b3dcd19247b7d4055fe1e
Ashish <ashishrawat2911@gmail.com>
2023-09-16 11:18:26 +0530

    ♻️ refactor: Refactor

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const dbConfig = {
     host: process.env.DB_HOST ?? "localhost",
     user: process.env.DB_USER ?? "root",
-    password: process.env.DB_PASSWORD ?? "",
+    password: process.env.DB_PASSWORD ?? " ",
     database: process.env.DB_DATABASE ?? "firekart",
     namedPlaceholders: true,
     decimalNumbers: true,
